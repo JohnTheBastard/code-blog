6cce6635abae7dc70b43b60ec7c62c54ffb8feae
John Hearn <jhearn@gmail.com>
2015-12-01 23:42:31 -0800

    implemented eventListeners for filtering and sorting

--- js/blogModule.js
+++ js/blogModule.js
@@ -12,15 +12,52 @@ function Blog() {
     var byTitle = function(a, b)  { return b.title - a.title };
     var byTitleReversed = function(a, b)  { return a.title - b.title };
 
-    this.articles = [ ];    
+    var cantFind = function( element, array ) {
+	for ( var ii=0; ii < array.length; ii++ ) {
+	    if ( element == array[ii] ) {
+		return false;
+	    }
+	}
+	return true;
+    }
 
+    this.articles = [ ];
+    this.authors = [ ];
+    this.categories = [ ];
+    this.dates = [ ];
     
     this.init = function( rawData ) {
 	for( var ii=0; ii < rawData.length; ii++ ) {
 	    this.articles.push( new Article( blogData[ii] ) );
+
+	    if ( cantFind( blogData[ii].author, this.authors ) ) {
+		this.authors.push( blogData[ii].author );
+	    }
+	    if ( cantFind( blogData[ii].category, this.categories ) ) {
+		this.categories.push( blogData[ii].category );
+	    }
+
 	}
 	//this.sortArticlesByDate();
 	this.articles.sort(byDate);
+	for( var ii=0; ii < this.articles.length; ii++ ) {
+	    this.dates.push( this.articles[ii].publishedOn );
+	}
+	this.authors.sort();
+	this.categories.sort();
+
+	var $authors = $('#authors ul');
+	var $categories = $('#categories ul');
+	var $dates = $('#dates ul');
+
+
+	for( var ii=0; ii < this.authors.length; ii++ ) {
+	    $authors.append('<li><span class="author">' + this.authors[ii] + '</span></li>' );
+	}
+	for( var ii=0; ii < this.categories.length; ii++ ) {
+	    $categories.append('<li><span class="category">' + this.categories[ii] + '</span></li>' );
+	}
+
     }
 
 };
@@ -36,6 +73,29 @@ var BLOG_MODULE = (function() {
 	my.$anchor.append( my.blog.articles[ii].toHTML() );
     }
 
+
+    my.eventListeners = function() {
+	var menuItem = '';
+	var itemClass = '';
+	var $filters = $('#blogNav > ul > li > ul > li');
+	var button = '';
+	var $sortButtons = $('.btn-group > span');
+	
+	$filters.on('click', function() {
+	    menuItem = $(this).text();
+	    itemClass = $(this).children().attr('class');
+	    console.log(menuItem + " " + itemClass);
+	    my.blog.filterBy(menuItem, itemClass);
+	});
+
+	$sortButtons.on('click', function() {
+	    button = $(this).attr('id');
+	    console.log(button);
+	    my.blog.sortBy(button);
+	});
+    }			
+    my.eventListeners();
+    
     return my;
     
 })();
